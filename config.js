fdac6563373bb7308189d7461997830a0a8e5366
Kristers36 <74603121+Kristers36@users.noreply.github.com>
2026-09-23 14:46:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ window.ARCGIGUESS_CONFIG = {
 
     // SVARĪGI:
     // Tavā slānī lauks ir OBJECTID, nevis FID.
-    landmarkIdField: "OBJECTID",
+    landmarkIdField: "FID",
 
     // Foto URL atrodas laukā Photo.
     landmarkPhotoField: "Photo",
